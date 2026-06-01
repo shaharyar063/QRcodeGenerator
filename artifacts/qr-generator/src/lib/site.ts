5d9fb436a304449e3f9011e2833e1503b74aad47
shaharyar063 <fraz2723@gmail.com>
2026-06-01 22:23:59 +0500

    ndbncbnd

--- artifacts/qr-generator/src/lib/site.ts
+++ artifacts/qr-generator/src/lib/site.ts
@@ -1,4 +1,4 @@
-export const SITE_DOMAIN = "frreeqr-code-generator.com";
+export const SITE_DOMAIN = "freeqr-code-generator.com";
 export const SITE_ORIGIN = `https://${SITE_DOMAIN}`;
 export const SITE_EMAIL = `hello@${SITE_DOMAIN}`;
 export const SITE_NAME = "QR Code Generator";
